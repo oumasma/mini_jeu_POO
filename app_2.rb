fd6e51f68dbe5d8d9eb30b4601efb99cddf2ad9f
oumasma <oumasma93@hotmail.fr>
2020-07-22 00:39:53 +0400

    game_2.0

---
+++ app_2.rb
@@ -0,0 +1,81 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game' 
+require_relative 'lib/player'
+
+
+puts" 
+==========================================
+               ✯╾━╤デ╦︻✯\n
+Bienvenue sur ILS VEULENT TOUS MA POO !      
+Serez-vous le dernier survivant ?
+============================================"
+
+
+#def create_player
+	puts "Quel est ton prénom ?" #on crée unplyer humain
+	print ">" 
+	human_name = gets.chomp.to_s
+	human = HumanPlayer.new(human_name)
+	puts human.name
+#end
+
+#def create_enemies     #on crée des ennemis stocké dans un array
+	enemie1 = Player.new("Josiane")
+	enemie2 = Player.new("José")
+	enemies = [enemie1,enemie2]
+#end
+
+
+while human.life_points >0 && (enemie1.life_points > 0 || enemie2.life_points >0) #on crée une condition pour rester dans le jeu
+  puts
+  puts "***** Voici ton état *****"    
+	puts human.show_state 
+	puts "Quelle action veux-tu effectuer ? Tape la lettre indiquée : "  #on demande au player de faire un choix
+	puts "> a : chercher une meilleure arme"
+	puts "> s : chercher à se soigner"
+	puts
+	puts "attaquer un joueur en vue :"
+	puts "> 0 : #{enemie1.show_state}"
+	puts "> 1 : #{enemie2.show_state}" 
+	
+	choice = gets.chomp.to_s # le player choisit
+		
+		if choice == "a"
+			human.search_weapon
+		elsif choice == "s"
+			human.search_health_pack
+		elsif choice == "0"
+			human.attacks(enemie1)
+		else choice == "1"
+			human.attacks(enemie2)
+		end
+
+		if enemie2.life_points>0 && enemie1.life_points> 0  #la condition pour que les ennemis attaquent
+		puts
+		puts "***** Attention : Les ennemies t'attaquent ! *****"
+		puts
+		end
+
+		enemies.each do | enemie |   #un each tant que les ennemis sont en vie
+    	if enemie.life_points > 0
+      	enemie.attacks(human)
+      end
+      if enemie.life_points <= 0
+      	break
+      end
+    end
+end
+
+
+puts "La partie est finie"   #la partie est finie 
+	if human.life_points > 0 || ennemies.life_points <= 0
+		puts "BRAVO ! TU AS GAGNE FACE AUX MECHANTS"
+	else
+		puts "Oh non ! Tu es dead : tu as perdu !"
+	end
+
+puts "============================================"
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -32,3 +32,74 @@ class Player   #classe qui modélise un joueur
 			return rand(1..6)
 	end
 end
+
+class HumanPlayer < Player #classe jouée par un humain, hérite de la classe Player 
+attr_accessor :weapon_level, :life_points #attribut qui definit le niveau de l'arme de HumanPlayer
+
+	def initialize (name) #que le nom en entrée
+		@life_points = 100
+		@weapon_level = 1 #integer
+	end
+
+	def show_state  #méthode qui va aficher l'état du joueur et de son arme
+		puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}" #le nombre de points de vie et niveau de l'arme
+	end
+
+	def compute_damage
+    rand(1..6) * @weapon_level # on mulitplie les dégats 
+  end
+
+  def search_weapon     # methode pour chercher un nouvelle arme
+  	new_weapon = rand(1..6)   # une valeur aléatoire pour la nouvelle arme
+  		puts "tu as trouvé une arme de niveau #{new_weapon} !"
+  	if new_weapon > @weapon_level   # on cherche si la nouvelle arme est meilleure
+  		puts "Super ! elle est meilleure que ton arme actuelle : prends-là."
+  		new_weapon
+  	else
+  		puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+  	end
+  end
+
+  def search_health_pack    # methode pour chercher un pack de vie
+  	health_pack = rand(1..6)   # une valeur aleatoire pour le pack de vie
+  	
+  	if health_pack == 1  # on augmente rien
+  		puts "pff...tu n'as rien trouvé"
+  	
+  	elsif health_pack >= 2 && health_pack <= 5   # si le pack est compris entre 2 et 5 inclus
+  		puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+  		if @life_points >= 50    # on veut que la vie ne depasse pas 100 : 
+  			@life_points = 100   # si sa vie initiale depasse 50 --> on la met à 100
+  		else
+  		@life_points = @life_points + 50 # sinon on l'augmente de 50 points
+  		end
+  	
+  	elsif health_pack ==6 #si le pack est tombe sur 6
+  		puts "Waow, tu as trouvé un pack de +80 points de vie !"
+  		if @life_points >= 20   # on veut que la vie ne depasse pas 100 :
+  			@life_points = 100 #si sa vie initiale depasse 20 --> on la met à 100
+  		else 
+  		@life_points = @life_points + 80 # sinon on l'augmente de 80 points
+  		end
+  	end
+  end	
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
